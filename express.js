6377cb743eb374f23f035bb92a372b2ef19fc048
Faride Tari <faridetari@gmail.com>
2024-06-08 17:49:50 -0700

    added default path for API

--- express.js
+++ express.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import morgan from 'morgan';
 import cors from 'cors';
+import apiRouter from './routes/api-router.js'
 const app = express();
 // Express middleware
 app.use(express.json());
@@ -15,4 +16,5 @@ app.use(cors());
 app.get('/', (req, res) => {
 res.send('Node.js Server is live!');
 });
+app.use('/v1', apiRouter);
 export default app;

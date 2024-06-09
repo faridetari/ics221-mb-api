420c4de413fae8f528e369ccbb0a4dd63f43d7ed
Faride Tari <faridetari@gmail.com>
2024-06-08 18:32:05 -0700

    message-schema.js

---
+++ models/message-schema.js
@@ -0,0 +1,19 @@
+import yup from 'yup';
+// Data Schema for a New Message
+// Matches the one from the front-end App!
+const messageSchema = yup.object().shape({
+name: yup
+.string()
+.trim()
+.min(3, 'Your name must be at least ${min} characters.')
+.max(15, 'Your name cannot be more than ${max} characters.')
+.matches(/^[A-Za-z0-9_]+$/, 'Invalid name. Use upper or lower caseletters, 0 to 9, or underscore only.')
+.required('Your name is required.'),
+msgText: yup
+.string()
+.trim()
+.min(2, 'Your message must be at least ${min} characters.')
+.max(30, 'Your message must be no more than ${max} characters')
+.required('A message is required.')
+});
+export default messageSchema;

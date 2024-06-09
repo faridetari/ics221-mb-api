ad793c9db890de5b3df085abc5283a67d5398eb7
Faride Tari <faridetari@gmail.com>
2024-06-08 17:52:19 -0700

    add api routes for GET and POST to /messages

---
+++ routes/api-router.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import { getAllMessages, addNewMessage } from '../controllers/msg-api-controller.js';
+const router = express.Router();
+router.route('/messages')
+.get(getAllMessages)
+.post(addNewMessage);
+export default router;

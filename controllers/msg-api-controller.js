c62286a1941d6180e4083b6b87d904b8e5b561c4
Faride Tari <faridetari@gmail.com>
2024-06-08 18:06:12 -0700

    Moved the array

--- controllers/msg-api-controller.js
+++ controllers/msg-api-controller.js
@@ -1,3 +1,14 @@
+
+
+const messages = [
+    { id: "1", myName: "Bill", mySentence: "Hi All!" },
+    { id: "2", myName: "Ann", mySentence: "ICS 221 is fun!" },
+    { id: "3", myName: "Johnny", mySentence: "I'm Stranded" },
+    { id: "4", myName: "Barb", mySentence: "Hi" },
+    { id: "5", myName: "Frank", mySentence: "Who's tried?" },
+    { id: "6", myName: "Sarah", mySentence: "I heart React" },
+  ];
+  
 // GET Request Handler
 const getAllMessages = (req, res) => {
     res.status(200).send('Successful API GET Request');

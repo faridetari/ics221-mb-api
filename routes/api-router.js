3dd6dfa8b435b7d4859403675bc0a5f4446cb60e
Faride Tari <faridetari@gmail.com>
2024-07-22 18:17:59 -0700

    protected the POST Request for the /messages endpoint

--- routes/api-router.js
+++ routes/api-router.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import passport from 'passport';
 import { getAllMessages, addNewMessage } from '../controllers/msg-api-controller.js';
 import { registerNewUser } from '../controllers/user-api-controller.js'; // Import the handler for the /users POST request
 
@@ -6,7 +7,7 @@ const router = express.Router();
 
 router.route('/messages')
     .get(getAllMessages)
-    .post(addNewMessage);
+    .post(passport.authenticate('basic', { session: false }), addNewMessage); // Modify to use Basic Authentication
 
 router.route('/users')
     .post(registerNewUser); // Add the /users route for POST requests
